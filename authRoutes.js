7dd37adb7478b95c20973a207b85bddd17542a00
pranav1401-kumar <91305074+pranav1401-kumar@users.noreply.github.com>
2024-07-29 09:06:41 +0530

    Add files via upload

--- authRoutes.js
+++ authRoutes.js
@@ -51,7 +51,7 @@ router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
     // Successful authentication, redirect to the dashboard
-    res.redirect('/dashboard');
+    res.redirect('https://vooshfrontend.vercel.app/dashboard');
   }
 );
 
